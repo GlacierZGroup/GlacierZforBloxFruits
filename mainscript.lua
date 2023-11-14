7a207d2d72df468bed95dcc306c694aa165d5257
Frost_ <90537774+Fr0stYT@users.noreply.github.com>
2023-11-14 11:01:51 -0600

    Update mainscript.lua

--- mainscript.lua
+++ mainscript.lua
@@ -1,15 +1,85 @@
-local GlacierZFolder = game.Players.LocalPlayer:WaitForChild("PlayerGui"):FindFirstChild("GlacierZ") or Instance.new("Folder")
-GlacierZFolder.Name = "GlacierZ"
-GlacierZFolder.Parent = game.Players.LocalPlayer:WaitForChild("PlayerGui")
+-- Function to create an iceberg-shaped GUI with specified properties
+local function createIcebergGUI(position, size, color, text)
+    local gui = Instance.new("ScreenGui")
+    gui.Name = text
+    gui.Parent = game.Players.LocalPlayer:WaitForChild("PlayerGui")
 
-local CustomModulesFolder = GlacierZFolder:FindFirstChild("CustomModules") or Instance.new("Folder")
-CustomModulesFolder.Name = "CustomModules"
-CustomModulesFolder.Parent = GlacierZFolder
+    local frame = Instance.new("Frame")
+    frame.Size = size
+    frame.Position = position
+    frame.BackgroundColor3 = color
+    frame.Parent = gui
+
+    local label = Instance.new("TextLabel")
+    label.Text = text
+    label.Parent = frame
+
+    return gui, frame
+end
+
+-- Function to make GUIs draggable
+local function makeDraggable(gui, frame)
+    local dragging
+    local dragInput
+    local dragStart
+    local startPos
+
+    local function update(input)
+        local delta = input.Position - dragStart
+        gui.Position = UDim2.new(startPos.X.Scale, startPos.X.Offset + delta.X, startPos.Y.Scale, startPos.Y.Offset + delta.Y)
+    end
+
+    frame.InputBegan:Connect(function(input)
+        if input.UserInputType == Enum.UserInputType.MouseButton1 then
+            dragging = true
+            dragStart = input.Position
+            startPos = gui.Position
+
+            input.Changed:Connect(function()
+                if input.UserInputState == Enum.UserInputState.End then
+                    dragging = false
+                end
+            end)
+        end
+    end)
+
+    frame.InputChanged:Connect(function(input)
+        if input.UserInputType == Enum.UserInputType.MouseMovement then
+            dragInput = input
+        end
+    end)
+
+    frame.InputEnded:Connect(function(input)
+        if input.UserInputType == Enum.UserInputType.MouseButton1 then
+            dragging = false
+        end
+    end)
+
+    game:GetService("RunService").RenderStepped:Connect(function()
+        if dragging then
+            update(dragInput)
+        end
+    end)
+end
+
+-- Function to create a toggle button
+local function createToggleButton(parent, position, size, color, text, onClick)
+    local button = Instance.new("TextButton")
+    button.Parent = parent
+    button.Position = position
+    button.Size = UDim2.new(size, 0, size, 0)
+    button.BackgroundColor3 = color
+    button.Text = text
+
+    button.MouseButton1Click:Connect(onClick)
+
+    return button
+end
 
 -- Function to save the toggled buttons and script functions to a JSON file
 local function saveConfig()
     local config = {}
-    for _, button in pairs(GlacierZFolder:GetChildren()) do
+    for _, button in pairs(workspace.GlacierZ:GetChildren()) do
         if button:IsA("TextButton") then
             local entry = {
                 ButtonName = button.Text,
@@ -35,8 +105,12 @@ local function downloadAssets()
     local assetFolder = game.Players.LocalPlayer:WaitForChild("PlayerGui"):GetAttribute("AssetFolder")
     local glacierZAssets = {
         CustomModule = {
-            FileName = "2753915549.lua",
-            Destination = CustomModulesFolder,
+            FileName = "CustomModules.lua",
+            Destination = workspace.GlacierZ,
+        },
+        ButtonAsset = {
+            FileName = "ButtonAsset.lua",
+            Destination = workspace,
         },
         -- Add more assets as needed
     }
@@ -47,14 +121,33 @@ local function downloadAssets()
         end)
 
         if success then
-            local moduleScript = Instance.new("ModuleScript")
-            moduleScript.Name = asset.FileName:gsub(".lua", "")
-            moduleScript.Source = content
-            moduleScript.Parent = asset.Destination
+            local newScript = Instance.new("Script")
+            newScript.Name = asset.FileName:gsub(".lua", "")
+            newScript.Source = content
+            newScript.Parent = asset.Destination
+
+            -- Check if it's a button asset and create it in the workspace
+            if asset.FileName == "ButtonAsset.lua" then
+                local button = Instance.new("TextButton")
+                button.Parent = asset.Destination
+                button.Name = "DownloadedButton"
+                button.Size = UDim2.new(0.2, 0, 0.1, 0)
+                button.Position = UDim2.new(0.5, 0, 0.5, 0)
+                button.BackgroundColor3 = Color3.new(0.7, 0.8, 1)
+                button.Text = "Downloaded Button"
+            end
         else
             warn("Failed to download asset:", asset.FileName)
         end
     end
+end
+
+-- Load and execute CustomModules.lua
+local customModules = workspace.GlacierZ:FindFirstChild("CustomModules")
+if customModules and customModules:IsA("Script") then
+    pcall(function()
+        loadstring(customModules.Source)()
+    end)
 end
 
 -- Set up the four icebergs with specified properties
@@ -85,4 +178,3 @@ downloadAssets()
 
 -- Save the config when the game closes
 game:BindToClose(saveConfig)
-
